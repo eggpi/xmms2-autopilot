e09a2ba65f03331a53fefd304b074a485780fd83
Guilherme Gonçalves <guilherme.p.gonc@gmail.com>
2012-12-23 12:31:29 -0200

    Refactored test code.

--- test_recommend.py
+++ test_recommend.py
@@ -1,10 +1,12 @@
 import recommend
 
+import networkx
+
 import random
 import unittest
 
 """
-Some non-extensive tests for requirements.py
+Some non-extensive tests for recommend.py
 """
 
 def fresh_random(mth):
@@ -15,28 +17,23 @@ def fresh_random(mth):
 
 class TestRecommend(unittest.TestCase):
     def setUp(self):
-        import networkx
+        default_graph_edges = [
+            (1, 2, {"weight": 2.0}),
+            (2, 1, {"weight": 1.0}),
+            (1, 3, {"weight": 3.0}),
+            (3, 4, {"weight": 1.0}),
+            (3, 5, {"weight": 1.0}),
+            (3, 6, {"weight": 2.0}),
+            (6, 2, {"weight": 1.0}),
+            (2, 7, {"weight": 1.0}),
+            (7, 8, {"weight": 1.0}),
+            (7, 9, {"weight": 1.0}),
+            (7, 10, {"weight": 1.0}),
+            (10, 11, {"weight": 1.0})
+        ]
 
-        g = networkx.DiGraph()
-
-        g.add_edge(1, 2, weight = 2)
-        g.add_edge(2, 1, weight = 1)
-        g.add_edge(1, 3, weight = 3)
-        g.add_edge(3, 4, weight = 1)
-        g.add_edge(3, 5, weight = 1)
-        g.add_edge(3, 6, weight = 2)
-        g.add_edge(6, 2, weight = 1)
-        g.add_edge(2, 7, weight = 1)
-        g.add_edge(7, 8, weight = 1)
-        g.add_edge(7, 9, weight = 1)
-        g.add_edge(7, 10, weight = 1)
-        g.add_edge(10, 11, weight = 1)
-
-        recommend._graph = g
-        recommend.MIN_GRAPH_SIZE = 0
-        recommend.MIN_CANDIDATES = 0
-        recommend.GRAPH_DOT_FILE = None
-        recommend.GRAPH_PERSISTENCE_FILE = None
+        recommend._graph = networkx.DiGraph()
+        recommend._graph.add_edges_from(default_graph_edges)
 
     def test_get_min_weight_neighbor(self):
         self.assertEquals(recommend._get_min_weight_neighbor(2, "in"), 6)
@@ -55,6 +52,7 @@ class TestRecommend(unittest.TestCase):
     @fresh_random
     def test_compute_candidates(self):
         # no climbing, last item is the random choice
+        recommend.MIN_CANDIDATES = 0
         self.assertEquals(recommend._compute_candidates(1, 1), {2: 2.0, 3: 3.0, 4: 2.0})
         self.assertEquals(recommend._compute_candidates(1, 2), {2: 2.0, 3: 3.0, 4: 0.5, 5: 0.5, 6: 1.0, 7: 0.5, 8: 0.5})
         self.assertEquals(recommend._compute_candidates(1, 3), {2: 2.0, 3: 3.0, 4: 0.5, 5: 0.5, 6: 1.0, 7: 0.5, 8: 1.0 / 3, 9: 1.0 / 3, 10: 1.0 / 3, 11: 1.0 / 3})
@@ -63,23 +61,30 @@ class TestRecommend(unittest.TestCase):
 
         # exercise climbing, last two items are the random choices for the
         # parent and the node
-        min_candidates, recommend.MIN_CANDIDATES = recommend.MIN_CANDIDATES, 3
+        recommend.MIN_CANDIDATES = 3
         self.assertEquals(recommend._compute_candidates(5, 1), {4: 1.0, 6: 2.0, 7: 1.0, 11: 1.0})
         self.assertEquals(recommend._compute_candidates(11, 2), {3: 1.0, 7: 1.0})
-        recommend.MIN_CANDIDATES = min_candidates
 
     @fresh_random
     def test_next(self):
+        recommend.MIN_CANDIDATES = 0
         self.assertEquals(recommend.next(1, 2), 3)
+
+        # add node to the graph, return random node
         self.assertEquals(recommend.next(20), 11)
+        self.assertTrue(recommend._graph.has_node(20))
 
     @fresh_random
     def test_min_candidates(self):
-        min_candidates, recommend.MIN_CANDIDATES = recommend.MIN_CANDIDATES, float("inf")
+        recommend.MIN_CANDIDATES = float("inf")
         self.assertEquals(recommend.next(10, 3), None)
+
         recommend.MIN_CANDIDATES = 0
         self.assertEquals(recommend.next(10, 3), 11)
-        recommend.MIN_CANDIDATES = min_candidates
 
 if __name__ == "__main__":
+    recommend.GRAPH_DOT_FILE = None
+    recommend.GRAPH_PERSISTENCE_FILE = None
+    recommend.MIN_GRAPH_SIZE = 0
+
     unittest.main()
